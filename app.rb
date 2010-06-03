f638753b08bc6e439bf96e07883262359e064d40
Brett Goulder <brett.goulder@gmail.com>
2010-06-03 15:55:56 +1000

    Helpers moved out

--- app.rb
+++ app.rb
@@ -10,7 +10,17 @@ module BrettGoulder
       enable :static
     
       helpers do
-        include Helpers
+        def partial(template, locals={})
+          haml("_#{template}".to_sym, :locals => locals, :layout => false)
+        end
+
+        def portfolio_path(portfolio)
+          "/portfolio/#{portfolio.slug}"
+        end
+
+        def render_page(portfolio)
+          Tilt.new(portfolio.path).render(self)
+        end
       end
     
       get '/' do
